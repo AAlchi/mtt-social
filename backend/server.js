5e01a47af39287a3b0f4cc2d2f147eaabffd63eb
Ali Ibrahim <alialchi07@gmail.com>
2023-02-25 11:53:52 -0600

    Test

--- backend/server.js
+++ backend/server.js
@@ -27,11 +27,7 @@ const userRouter = require('./routes/userRoutes.js')
 
 app.use('/', userRouter);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
